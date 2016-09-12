732c6fc1efe64c7fb024c4e575adf7ff1e08495a
Norbert Sram <norbert.sram@adnovum.hu>
2016-09-12 12:55:39 +0200

    Support for org-agenda based task creation.

--- module/my-org.el
+++ module/my-org.el
@@ -7,7 +7,11 @@
 (use-package org
   :ensure t
   :bind (("C-c o a" . org-agenda))
-  :config (setq org-agenda-files (list "~/Documents/org")))
+  :config (setq org-agenda-files (list "~/Documents/org/")
+		org-default-notes-file "~/Documents/org/adhoc.org"
+		org-capture-templates
+		(quote (("t" "Todo" entry (file "~/Documents/org/adhoc.org") "* TODO %^{Todo description} %? Added: %U %i %a")
+			("d" "Scheduled task" entry (file "~/Documents/org/adhoc.org") "* TODO %^{Task description} %? \nSCHEDULED: %t %i %a")))))
 
 (provide 'my-org)
 ;;; my-org.el ends here
