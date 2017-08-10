e0da4b890edc88217c0316a3c4f48844c95b0688
Norbert Sram <sram@ms.adnovum.ch>
2017-08-10 10:43:23 +0200

    Made org setup work on Windows as well.

--- module/my-org.el
+++ module/my-org.el
@@ -4,15 +4,21 @@
 
 ;;; Code:
 
+(defvar my-org-folder-path "~/Documents/org")
+
+(when (string-equal system-type "windows-nt") (setq my-org-folder-path "~/../../Dropbox/org"))
+
+(defvar my-org-notes-file-path (concat my-org-folder-path "/notes.org"))
+
 (use-package org
   :ensure t
   :bind (("C-c o a" . org-agenda))
   :config (progn
-	    (setq org-agenda-files (list "~/Documents/org/")
-		  org-default-notes-file "~/Documents/org/notes.org"
+	    (setq org-agenda-files (list my-org-folder-path)
+		  org-default-notes-file my-org-notes-file-path
 		  org-capture-templates
-		  (quote (("t" "Todo" entry (file "~/Documents/org/notes.org") "* TODO %^{Todo description} %? Added: %U %i %a")
-			  ("d" "Scheduled task" entry (file "~/Documents/org/notes.org") "* TODO %^{Task description} %? \nSCHEDULED: %t %i %a"))))
+		  (quote (("t" "Todo" entry (file my-org-notes-file-path) "* TODO %^{Todo description} %? Added: %U %i %a")
+			  ("d" "Scheduled task" entry (file my-org-notes-file-path) "* TODO %^{Task description} %? \nSCHEDULED: %t %i %a"))))
 	    (use-package org-bullets
 	      :ensure t
 	      :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))))
