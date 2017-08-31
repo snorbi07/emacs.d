d3a2bb5b7112e0a32df66d9dc5f6425b4e1fefc2
Norbert Sram <norbert.sram@adnovum.hu>
2017-08-31 10:50:20 +0200

    shit

--- init.el
+++ init.el
@@ -399,3 +399,17 @@
   :load-path "module/")
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (web-mode yaml-mode which-key use-package undo-tree tidy tide smartparens smart-mode-line realgud paredit org-bullets neotree meghanada markdown-mode magit key-chord imenu-anywhere idea-darkula-theme highlight-symbol helm-swoop helm-projectile helm-descbinds helm-ag groovy-mode expand-region elpy elixir-mode crux company-quickhelp ag adoc-mode ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight semi-bold :height 98 :width normal)))))
