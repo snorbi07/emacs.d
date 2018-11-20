60ff12618d8132bcbff2e419b61c0e799af07766
Norbert Sram <me@norbertsram.com>
2018-11-20 21:23:44 +0100

    Added web-mode for various templating hooks

--- init.el
+++ init.el
@@ -447,6 +447,30 @@
   )
 
 
+;; Support for various template formats
+(use-package web-mode
+  :ensure t
+  :defer t
+  :config
+  (progn
+    ;; adjust indents for web-mode to 2 spaces
+    (defun my-web-mode-hook ()
+      "Hooks for Web mode.  Adjust indent."
+      (setq web-mode-markup-indent-offset 2
+	    web-mode-css-indent-offset 2
+	    web-mode-code-indent-offset 2))
+    (add-hook 'web-mode-hook  'my-web-mode-hook)
+    ;; add hooks for everything!
+    (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+  )
+
+
 ;; Scala support (This assumes that SBT is on the path)
 (use-package ensime
   :ensure t
