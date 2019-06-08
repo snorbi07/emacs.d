8115e2f6588a13577d599e00ce0f40ffc4e7bcb0
Norbert Sram <norbert.sram@goodgrind.tech>
2019-06-08 19:33:28 +0200

    Org GTD setup improvements

--- module/my-org.el
+++ module/my-org.el
@@ -8,17 +8,23 @@
 
 (when (string-equal system-type "windows-nt") (setq my-org-folder-path "~/../../Dropbox/org"))
 
-(defvar my-org-notes-file-path (concat my-org-folder-path "/notes.org"))
+(defvar my-org-notes-file-path (concat my-org-folder-path "/gtd/refile.org"))
 
 (use-package org
   :ensure t
-  :bind (("C-c o a" . org-agenda))
+  :mode ("\\.org\\'" . org-mode)
+  :bind (("C-c o a" . org-agenda)
+	 ("C-c c" . org-capture)
+	 :map org-mode-map
+	 ("C-c C-r" . org-refile))
   :config (progn
-	    (setq org-agenda-files (list my-org-folder-path)
+	    (setq org-agenda-files (list (concat my-org-folder-path "/gtd"))
+		  org-refile-use-outline-path t
+		  org-refile-targets  '((org-agenda-files :maxlevel . 3))
 		  org-default-notes-file my-org-notes-file-path
 		  org-capture-templates
-		  (quote (("t" "Todo" entry (file my-org-notes-file-path) "* TODO %^{Todo description} %? Added: %U %i %a")
-			  ("d" "Scheduled task" entry (file my-org-notes-file-path) "* TODO %^{Task description} %? \nSCHEDULED: %t %i %a"))))
+		  (quote (("t" "TODO" entry (file+headline my-org-notes-file-path "Captures") "* TODO %^{Todo description}")
+			  ("T" "TODO" entry (file+headline my-org-notes-file-path "Captures") "* TODO %^{Todo description}\n %? Added: %U %i %a"))))
 	    (use-package org-bullets
 	      :ensure t
 	      :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))))
